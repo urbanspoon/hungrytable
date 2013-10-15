e04134b35b6ad7111142039c38e3cd93b6df825c
Joseph Chen <joseph@urbanspoon.com>
2013-10-15 11:58:50 -0700

    Allowing early and later time slots to be locked

--- lib/hungrytable/restaurant_slotlock.rb
+++ lib/hungrytable/restaurant_slotlock.rb
@@ -2,11 +2,12 @@ module Hungrytable
   class RestaurantSlotlock
     include RequestExtensions
 
-    attr_reader :restaurant_search
+    attr_reader :restaurant_search, :time_slot
 
-    def initialize restaurant_search, requester=PostRequest
+    def initialize restaurant_search, requester=PostRequest, time_slot=nil
       @restaurant_search = restaurant_search
       @requester         = requester
+      @time_slot         = time_slot
     end
 
     def successful?
@@ -23,11 +24,27 @@ module Hungrytable
     end
 
     def params
+      if time_slot.nil? || time_slot == :ideal
+        time = restaurant_search.ideal_time
+        security_id = restaurant_search.ideal_security_id
+      elsif time_slot == :exact
+        time = restaurant_search.exact_time
+        security_id = restaurant_search.exact_security_ID
+      elsif time_slot == :later
+        time = restaurant_search.later_time
+        security_id = restaurant_search.later_security_ID
+      elsif time_slot == :early
+        time = restaurant_search.early_time
+        security_id = restaurant_search.early_security_ID
+      else
+        raise "Unrecognized time slot parameter: #{time_slot}"
+      end
+
       {
         'RID'            => restaurant.id,
-        'datetime'       => restaurant_search.ideal_time,
+        'datetime'       => time,
         'partysize'      => restaurant_search.party_size,
-        'timesecurityID' => restaurant_search.ideal_security_id,
+        'timesecurityID' => security_id,
         'resultskey'     => restaurant_search.results_key
       }
     end
