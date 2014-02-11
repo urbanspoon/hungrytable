f0c8c9448b0b3c6092d3663650ec9b5406b0e3eb
Brian Roberts <brianroberts@Brians-MacBook-Pro.local>
2014-02-11 08:31:17 -0800

    search updates

--- lib/hungrytable/restaurant_search.rb
+++ lib/hungrytable/restaurant_search.rb
@@ -3,33 +3,39 @@ module Hungrytable
     include RequestExtensions
     include ResponseAccessors
 
-    def initialize params
+    def initialize params, requester = GetRequest
       @params = params
+      @requester = requester
       ensure_required_params
-      @requester = GetRequest
     end
 
     def timeslots
       response['Timeslots']
     end
 
+    def results_key
+      response['ResultsKey']
+    end
+
     private
     def params
-      dt = @params[:date_time]
-      dt = encode_date_time(dt) if dt.respond_to?(:strftime)
       {
         rid: @params[:restaurant_id],
-        dt: dt,
+        dt: formatted_date_time,
         ps: @params[:party_size]
       }
     end
 
-    def required_params
-      %w(restaurant_id date_time party_size).map(&:to_sym)
+    def formatted_date_time
+      @params[:date_time].respond_to?(:strftime) ? encoded_date_time : @params[:date_time]
     end
 
-    def encode_date_time dt
-      URI.encode(dt.strftime("%m/%d/%Y %I:%M %p"), /[^a-z0-9\-\.\_\~]/i)
+    def encode_date_time
+      URI.encode(@params[:date_time].strftime('%Y-%m-%dT%H:%M:%S'), /[^a-z0-9\-\.\_\~]/i)
+    end
+
+    def required_params
+      %w(restaurant_id date_time party_size).map(&:to_sym)
     end
 
     def request_uri
